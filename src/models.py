0de4a80a7657a17d25c20070c22ecd315606c165
ShayaKahn <shayakan9@gmail.com>
2025-03-18 16:05:21 +0200

    Updates.

--- src/models.py
+++ src/models.py
@@ -13,6 +13,34 @@ from xgboost import dask as dxgb
 import dask.dataframe as dd
 from src.utils import *
 
+####
+import pandas as pd
+import numpy as np
+import os
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+from scipy.stats import normaltest
+import seaborn as sns
+import tabulate
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from scipy.stats import randint as sp_randint
+from dask.distributed import Client, LocalCluster
+from xgboost import dask as dxgb
+import dask.dataframe as dd
+from imblearn.pipeline import Pipeline
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler, SMOTE
+from sklearn.metrics import f1_score, precision_score, recall_score
+import warnings
+import time
+import plotly.offline as pyo
+pyo.init_notebook_mode(connected=True)
+warnings.filterwarnings("ignore", category=FutureWarning)
+####
+
 def create_ANN_model(input_dimension, num_classes, num_layers, num_neurons,
                      learning_rate, weight_decay, activation, dropout_rate=0.2):
     """Create an Artificial Neural Network model.
@@ -271,5 +299,3 @@ def xgbb(X_train, y_train, X_test, y_test, best_params, random_state, desired_co
     score = f1_score(y_test_corrected, y_pred, average='macro')
 
     return model, score, y_pred
-
-
